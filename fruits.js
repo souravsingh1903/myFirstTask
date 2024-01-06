3ea977e56d225ad653c9ccc1fd5ac3e6fe9413f5
souravsingh1903 <souravsingh1903@gmail.com>
2024-01-06 16:04:39 +0530

    querySelector done

--- fruits.js
+++ fruits.js
@@ -26,3 +26,40 @@ list[3].style.color  ='blue';
 for(let i =0; i<list.length;i++){
     list[i].style.fontStyle = 'italic';
 }
+// Write the code as shown in the video below:
+//  QuerySelector and QuerySelectorAll usd
+const mainHeading = document.querySelector('#main-heading');
+mainHeading.style.textAlign = 'right';
+
+
+const basketHeading = document.querySelector('#basket-heading');
+basketHeading.style.color = 'brown';
+basketHeading.style.marginLeft = '30px';
+
+const fruits =  document.querySelector('.fruits');
+fruits.style.backgroundColor = 'gray';
+fruits.style.margin  ='30px';
+fruits.style.padding  ='30px';
+fruits.style.width  ='50%';
+fruits.style.borderRadius = '5px';
+fruits.style.listStyleType = 'none';
+
+const fruit =  document.querySelectorAll('.fruit');
+for(let i =0; i<fruit.length; i++){
+  fruit[i].style.backgroundColor = 'white';
+  fruit[i].style.margin = '10px';
+  fruit[i].style.padding = '10px';
+  fruit[i].style.borderRadius = '5px';
+}
+const oddSelectors = document.querySelectorAll('.fruit:nth-child(odd)');
+for(let i =0; i<oddSelectors.length; i++){
+  oddSelectors[i].style.backgroundColor = 'lightgray';
+  }
+const evenSelectors = document.querySelectorAll('.fruit:nth-child(even)');
+  for(let i =0; i<evenSelectors.length; i++){
+  evenSelectors[i].style.backgroundColor = 'red';
+     evenSelectors[i].style.color = 'white';
+  }                                      
+
+
+// Write answer to the questions asked below:

0433efdf5d982cb88505b9ca970c0e8a7515fa0e
souravsingh1903 <souravsingh1903@gmail.com>
2024-01-06 14:40:15 +0530

    changesInId

---
+++ fruits.js
@@ -0,0 +1,16 @@
+// Write your code below:
+const mainHeading = document.getElementById('main-heading');
+mainHeading.textContent = "Fruit World";
+mainHeading.style.color =  'orange';
+
+const mainHeader = document.getElementById('header'); 
+mainHeader.style.backgroundColor ='green';
+
+mainHeader.style.borderBottom ='3px solid orange';
+
+const basketHeading = document.getElementById('basket-heading');
+basketHeading.style.color = 'green';
+
+const greeting = document.getElementById('thanks');
+greeting.innerHTML ='<p>Please visit us again</p>';
+
